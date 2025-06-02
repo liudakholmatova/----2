77cba6e681408d1c7374d61e687339a0fdacaef7
Людмила Бесащук <besasukludmila@gmail.com>
2025-06-02 13:18:42 +0300

    підключення js

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Swiper(".swiper", {
-    loop: true,      // Безкінечний слайдер
-    navigation: {    // Кнопки навігації
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-});
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+
+
+
+
+//   new Swiper(".swiper", {
+//     loop: true,      // Безкінечний слайдер
+//     navigation: {    // Кнопки навігації
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+// });
 //   let modal = document.getElementById("menuModal");
 //         let btn = document.getElementById("openModal");
 //         let span = document.getElementsByClassName("close")[0];
